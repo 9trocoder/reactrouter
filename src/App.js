e4ba4b3e8c9d134e59eee48c676263297f541d4f
yemicodes <yemicodes@gmail.com>
2021-02-18 16:59:16 +0300

    Add styling
    copy link from bootstrap website, add styling to header

--- src/App.js
+++ src/App.js
@@ -12,11 +12,16 @@ const App = () => {
 
     <BrowserRouter>
       <header>
-      <Link to="/">Home</Link>---
-      <Link to="/services">Services</Link>---
-      <Link to="/about">About</Link>---
-      <Link to="/contact">Contact</Link>
-      <hr/>
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+          <h5 className="my-0 mr-md-auto font-weight-normal">React Router</h5>
+          <nav className="my-2 my-md-0 mr-md-3">
+            <Link className="p-2 text-dark" to="/">Home</Link>
+            <Link className="p-2 text-dark" to="/services">Services</Link>
+            <Link className="p-2 text-dark" to="/about">About</Link>
+            <Link className="p-2 text-dark" to="/contact">Contact</Link>
+          </nav>
+        </div>
+      
     </header>
 
       <Route path="/" exact component={Home} />
